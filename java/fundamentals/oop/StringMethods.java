f6da922a9bba081a932f29c83872c84e1e81eb81
Abraham Oluwasemilore Bankole <semi.banks.317@gmail.com>
2025-07-05 22:12:37 +0100

    efficient methods

--- java/fundamentals/oop/StringMethods.java
+++ java/fundamentals/oop/StringMethods.java
@@ -1,7 +1,32 @@
 package fundamentals.oop;
 
+import java.util.Arrays;
+
 public class StringMethods {
     public static void main(String[] args) {
+        String str = "Hello World";
+        int length = str.length();
 
+        String word = str.substring(0);
+        System.out.println(word);
+
+        String firstWord = str.substring(0, 5);
+        System.out.println(firstWord);
+
+        String lastWord = str.substring(length - 5);
+        System.out.println(lastWord);
+
+        String e = String.valueOf(str.charAt(1));
+        System.out.println(e);
+
+        char ch = str.charAt(3);
+
+        int indexOfe = str.indexOf("l");
+        System.out.println(indexOfe);
+
+        String upper = str.toUpperCase();
+
+        char[] chars = word.trim().toCharArray();
+        System.out.println(Arrays.toString(chars));
     }
 }
