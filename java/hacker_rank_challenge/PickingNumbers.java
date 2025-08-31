5cdaa94f9064ceedfe969773dff3cc5cf5e8e855
Abraham Oluwasemilore Bankole <semi.banks.317@gmail.com>
2025-08-31 20:36:54 +0100

    made optimizations

--- java/hacker_rank_challenge/PickingNumbers.java
+++ java/hacker_rank_challenge/PickingNumbers.java
@@ -12,14 +12,15 @@ public class PickingNumbers {
         for (int number : a) {
             frequency_arr[number]++;
         }
-
+        // {0,0,1,2,2,3,3,3}
+        // {2,1,2,3}
         int maxLength = 0;
 
         // iterate through frequency_arr to find the maximum length subarray
         // by considering pairs of adjacent numbers (i and i+1).
         for (int i = 0; i < frequency_arr.length - 1; i++) {
-            // the sum of frequencies of i and i+1 represents a potential subarray
-            // where the absolute difference between any two elements is <= 1.
+            // the sum of frequencies of i and i+1 is a possible subarray
+            // the max difference between two values <= 1
             int currentLength = frequency_arr[i] + frequency_arr[i + 1];
             if (currentLength > maxLength) {
                 maxLength = currentLength;
